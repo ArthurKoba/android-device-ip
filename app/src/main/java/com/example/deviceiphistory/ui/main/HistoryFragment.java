73b94415547d766e90fea3b70f1e8d3b370f671d
Arthur Koba <caprimores@gmail.com>
2022-12-23 10:44:28 +0300

    add first state HistoryFragment

--- app/src/main/java/com/example/deviceiphistory/ui/main/HistoryFragment.java
+++ app/src/main/java/com/example/deviceiphistory/ui/main/HistoryFragment.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.deviceiphistory.ApiService;
 import com.example.deviceiphistory.R;
 
 
 public class HistoryFragment extends Fragment {
+    Button getHistoryButton;
+    Button clearHistoryButton;
+    ListView historyListView;
     ApiService apiService;
 
     public static HistoryFragment newInstance(Context context) {
@@ -33,10 +39,23 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
-//        initElements(view);
-//        initDefaultTexts();
-//        initHandlers();
+        getHistoryButton = view.findViewById(R.id.getHistory);
+        clearHistoryButton = view.findViewById(R.id.clearHistory);
+        historyListView = view.findViewById(R.id.historyList);
+        getHistoryButton.setOnClickListener(v -> getHistory());
+        clearHistoryButton.setOnClickListener(v -> clearHistory());
         return view;
     }
+
+    private void getHistory() {
+        Log.d("History", "click get");
+
+    }
+
+    private void clearHistory() {
+        Log.d("History", "click clear");
+    }
+
+
 
 }
