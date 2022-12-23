7922133b081584ca67fd465ae68a12dd08b0d90c
Arthur Koba <caprimores@gmail.com>
2022-12-23 10:22:42 +0300

    refactor TestFragment

--- app/src/main/java/com/example/deviceiphistory/ui/main/TestFragment.java
+++ app/src/main/java/com/example/deviceiphistory/ui/main/TestFragment.java
@@ -10,40 +10,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.deviceiphistory.ApiService;
 import com.example.deviceiphistory.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TestFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private boolean buttonActive = false;
 
     TextView testText;
     Button testButton;
+    ApiService apiService;
 
     public TestFragment() {
-
+        apiService = ApiService.getInstance();
     }
 
     public static TestFragment newInstance() {
-        String param1 = "par1";
-        String param2 = "par2";
-
         TestFragment fragment = new TestFragment();
         Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,30 +33,19 @@ public class TestFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        testText =
-//        tvInfo = (TextView)findViewById(R.id.textView);
-//        etInput = (EditText)findViewById(R.id.editTextNumber);
-//        Button button= (Button)findViewById(R.id.button);
-//        Button testButton = findViewById();
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_test, container, false);
         testText = view.findViewById(R.id.testText);
         testButton = view.findViewById(R.id.testButton);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                testText.setText("lol");
-            }
-        });
+        testButton.setOnClickListener(v -> click());
         return view;
+    }
+
+    private void click() {
+        testText.setText("lol");
     }
 }
